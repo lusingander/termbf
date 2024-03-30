e238b739b5cbcfff615b6aeb6d932fb0d63a7972
Kyosuke Fujimoto <kyoro.f@gmail.com>
2024-03-30 16:02:10 +0900

    Implement interpreter

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # termbf
+
+## Brainf\*ck interpreter specification
+
+- EOF returns 0
+- cell size is `u8`
+- pointer is `u8`
+- Exceeding the cell and pointer value range causes a runtime error
+- All characters other than the eight basic commands are ignored
--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,3 +1,115 @@
+use std::collections::HashMap;
+
+pub struct Interpreter {
+    memory: Vec<u8>,
+    ptr: u8,
+
+    tokens: Vec<Token>,
+    jump_map: HashMap<Token, usize>,
+    cur: usize,
+    input: String,
+    output: String,
+}
+
+impl Interpreter {
+    pub fn new(source: &str, input: &str) -> Interpreter {
+        let tokens = tokenize(source);
+        let jump_map = build_jump_map(&tokens);
+        Interpreter {
+            memory: vec![0; u8::MAX as usize],
+            ptr: 0,
+            tokens,
+            jump_map,
+            cur: 0,
+            input: input.to_string(),
+            output: String::new(),
+        }
+    }
+
+    pub fn input(&self) -> &str {
+        &self.input
+    }
+
+    pub fn output(&self) -> &str {
+        &self.output
+    }
+
+    pub fn current_line_and_pos(&self) -> (usize, usize) {
+        let t = self.current_token();
+        (t.line, t.pos)
+    }
+
+    pub fn end(&self) -> bool {
+        self.cur >= self.tokens.len()
+    }
+
+    pub fn step(&mut self) {
+        let token = self.current_token();
+        match token.tp {
+            TokenType::Plus => {
+                let v = self.current_value();
+                *v = v.checked_add(1).unwrap();
+                self.cur += 1;
+            }
+            TokenType::Minus => {
+                let v = self.current_value();
+                *v = v.checked_sub(1).unwrap();
+                self.cur += 1;
+            }
+            TokenType::RightAngle => {
+                self.ptr = self.ptr.checked_add(1).unwrap();
+                self.cur += 1;
+            }
+            TokenType::LeftAngle => {
+                self.ptr = self.ptr.checked_sub(1).unwrap();
+                self.cur += 1;
+            }
+            TokenType::LeftSquare => {
+                let v = self.current_value();
+                if *v == 0 {
+                    self.cur = self.jump_idx(&token);
+                } else {
+                    self.cur += 1;
+                }
+            }
+            TokenType::RightSquare => {
+                let v = self.current_value();
+                if *v != 0 {
+                    self.cur = self.jump_idx(&token);
+                } else {
+                    self.cur += 1;
+                }
+            }
+            TokenType::Dot => {
+                let v = self.current_value();
+                let c = *v as char;
+                self.output.push(c);
+                self.cur += 1;
+            }
+            TokenType::Comma => {
+                let mut cs = self.input.chars();
+                let c = cs.next().unwrap_or(0 as char); // EOF: 0
+                self.input = cs.collect();
+                let v = self.current_value();
+                *v = c as u8;
+                self.cur += 1;
+            }
+        }
+    }
+
+    fn current_token(&self) -> Token {
+        *self.tokens.get(self.cur).unwrap()
+    }
+
+    fn current_value(&mut self) -> &mut u8 {
+        self.memory.get_mut(self.ptr as usize).unwrap()
+    }
+
+    fn jump_idx(&self, token: &Token) -> usize {
+        *self.jump_map.get(token).unwrap()
+    }
+}
+
 fn tokenize(source: &str) -> Vec<Token> {
     let mut tokens = Vec::new();
     for (line, s) in source.lines().enumerate() {
@@ -25,7 +137,7 @@ fn char_to_token_type(c: char) -> Option<TokenType> {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Token {
     tp: TokenType,
     line: usize,
@@ -38,7 +150,7 @@ impl Token {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum TokenType {
     Plus,
     Minus,
@@ -48,6 +160,26 @@ enum TokenType {
     RightSquare,
     Dot,
     Comma,
+}
+
+fn build_jump_map(tokens: &[Token]) -> HashMap<Token, usize> {
+    let mut m = HashMap::new();
+    let mut stack: Vec<(&Token, usize)> = Vec::new();
+    for (i, t) in tokens.iter().enumerate() {
+        match t.tp {
+            TokenType::LeftSquare => {
+                stack.push((t, i));
+            }
+            TokenType::RightSquare => {
+                if let Some((tt, ii)) = stack.pop() {
+                    m.insert(*t, ii + 1);
+                    m.insert(*tt, i + 1);
+                }
+            }
+            _ => {}
+        }
+    }
+    m
 }
 
 #[cfg(test)]
@@ -76,5 +208,27 @@ abc .,
             Token::new(TokenType::RightAngle, 4, 13),
         ];
         assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_interpreter_hello_world() {
+        let source = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+        let output = run_interpreter(source, "");
+        assert_eq!(output, "Hello World!\n")
+    }
+
+    #[test]
+    fn test_interpreter_echo() {
+        let source = ",[.,]";
+        let output = run_interpreter(source, "Rust");
+        assert_eq!(output, "Rust")
+    }
+
+    fn run_interpreter(source: &str, input: &str) -> String {
+        let mut interpreter = Interpreter::new(source, input);
+        while !interpreter.end() {
+            interpreter.step()
+        }
+        interpreter.output
     }
 }
